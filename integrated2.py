d7e5d41abdd570becf099305acd4f98d2de28ee0
Pranesh Nangare <praneshnangare10@gmail.com>
2021-11-24 01:14:55 +0530

    Update integrated2.py

--- integrated2.py
+++ integrated2.py
@@ -480,6 +480,7 @@ import tensorflow as tf
 
 try:
   tpu = tf.distribute.cluster_resolver.TPUClusterResolver()  # TPU detection
+  print('Running on TPU ', tpu.cluster_spec().as_dict()['worker'])
 except ValueError:
   raise BaseException('ERROR: Not connected to a TPU runtime; please see the previous cell in this notebook for instructions!')
 
